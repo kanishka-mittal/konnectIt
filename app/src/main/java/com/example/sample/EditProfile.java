55f68bb421fc979c82df33e59b8c08c4494b0c0b
Kanishka Mittal <kanishka.mittal279@gmail.com>
2021-11-21 14:46:30 +0530

    edit profile page added

--- app/src/main/java/com/example/sample/EditProfile.java
+++ app/src/main/java/com/example/sample/EditProfile.java
@@ -2,13 +2,31 @@ package com.example.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class EditProfile extends AppCompatActivity {
-
+    private MaterialButton btnSubmit;
+    private int userId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            userId = extras.getInt("userId");
+        }
+        btnSubmit=findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(EditProfile.this,Notifications.class);
+                intent.putExtra("userId",userId);
+                startActivity(intent);
+            }
+        });
     }
 }
