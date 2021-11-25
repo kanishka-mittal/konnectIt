89af4dc6a2607238b2cebf40d6eb9ba85e3f0fb3
Kanishka Mittal <kanishka.mittal279@gmail.com>
2021-11-25 18:05:00 +0530

    few changes

--- app/src/main/java/com/example/sample/Profile.java
+++ app/src/main/java/com/example/sample/Profile.java
@@ -2,10 +2,15 @@ package com.example.sample;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -53,10 +58,38 @@ public class Profile extends AppCompatActivity {
         tabLayout.getTabAt(0).setIcon(R.drawable.info);
         tabLayout.getTabAt(1).setIcon(R.drawable.posticon);
 
+        String method="load";
         TextView Fullname = findViewById(R.id.fullname);
         TextView Username = findViewById(R.id.username);
         ProfileBackgroundTask bgTask=new ProfileBackgroundTask(this,userId, Fullname, Username, infopage);
-        bgTask.execute();
+        bgTask.execute(method);
     }
 
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.profilemenu, menu);
+
+        if(menu instanceof MenuBuilder){
+            MenuBuilder m = (MenuBuilder) menu;
+            m.setOptionalIconsVisible(true);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int item_id = item.getItemId();
+
+        if(item_id == R.id.editprofile){
+            String method2="edit";
+            TextView Fullname = findViewById(R.id.fullname);
+            TextView Username = findViewById(R.id.username);
+            Profile_info infopage = new Profile_info();
+            ProfileBackgroundTask bgTask=new ProfileBackgroundTask(this,userId, Fullname, Username, infopage);
+            bgTask.execute(method2);
+        }
+        return true;
+    }
 }
