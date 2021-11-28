0b9fb4eb51cf4731a3f4ad971fe7ab254b3c9949
Kanishka Mittal <kanishka.mittal279@gmail.com>
2021-11-28 16:20:32 +0530

    chat (:) (:)

--- app/src/main/java/com/example/sample/MessageListAdapter.java
+++ app/src/main/java/com/example/sample/MessageListAdapter.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
 
+import org.w3c.dom.Text;
+
 import java.util.List;
 
 public class MessageListAdapter extends ArrayAdapter<Message> {
@@ -35,14 +37,18 @@ public class MessageListAdapter extends ArrayAdapter<Message> {
             CardView sendCard=v.findViewById(R.id.sendCard);
             TextView recMsg=v.findViewById(R.id.recMsg);
             TextView sentMsg=v.findViewById(R.id.sentMsg);
+            TextView sendTime=v.findViewById(R.id.sendTime);
+            TextView recTime=v.findViewById(R.id.recTime);
             if(message.getSenderEmail().equals(email)){
                 recCard.setVisibility(View.GONE);
                 sendCard.setVisibility(View.VISIBLE);
                 sentMsg.setText(message.getMsgTxt());
+                sendTime.setText(message.getTimestamp());
             }else{
                 recCard.setVisibility(View.VISIBLE);
                 sendCard.setVisibility(View.GONE);
                 recMsg.setText(message.getMsgTxt());
+                recTime.setText(message.getTimestamp());
             }
 
 
